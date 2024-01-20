6add2769ca8514225d02dd8e75f5e5eb5d9d307f
magnushelliesen <magnus.helliesen@gmail.com>
2024-01-20 09:27:52 +0100

    Added factorial function

---
+++ factorial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    // If zero is reached, return 1
+    if (n == 0){
+        return 1;
+    }
+    // Otherwise, calculate factorial recursively
+    else{
+        return n*factorial(n-1);
+    }
+}
+
+int main() {
+    // Take input;
+    int n;
+    cout << "Choose a number:\n";
+    cin >> n;
+
+    // Calculate factorial;
+    int n_factorial = factorial(n);
+
+    // Return factorial;
+    cout << n << " factorial is:\n" << n_factorial << "\n";
+
+    return 0;
+}
