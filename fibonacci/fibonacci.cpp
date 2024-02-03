b332c03d33c8857729c2680c7d0c014fa6112b51
magnushelliesen <magnus.helliesen@gmail.com>
2024-02-03 07:45:19 +0100

    Added iterative function

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -3,23 +3,47 @@
 using namespace std;
 
 // Function that calculates n'th fibonnaci number using recursion
-long double fibonnaci(int n){
+long double fibonnaci_recursive(int n) {
     // If < 2 is reached, return n
     if (n < 2){
         return n;
     }
     // Otherwise, calculate fibonnaci number recursively
-    else{
-        return fibonnaci(n-1)+fibonnaci(n-2);
+    else {
+        return fibonnaci_recursive(n-1)+fibonnaci_recursive(n-2);
     }
+}
+
+// Function that calculates n'th fibonnaci number using iteration
+long double fibonnaci_iterative(int n) {
+    long double n0, n1, n2;
+    n1 = 1;
+    n2 = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i < 2){
+            n0 = i;
+        } else {
+            n0 = n1 + n2;
+            n2 = n1;
+            n1 = n0;
+        }
+    }
+    return n0;
 }
 
 int main(int argc, char* argv[]) {
     // Pass command line argument to n;
     int n = atoi(argv[1]);
+    int recursion = atoi(argv[2]);
 
     // Calculate fibonnaci number of order n;
-    long double result = fibonnaci(n);
+    long double result;
+    if (recursion == 1) {
+        result = fibonnaci_recursive(n);
+    }
+    else {
+        result = fibonnaci_iterative(n);
+    }
 
     // Print result;
     cout << result << "\n";
--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from decimal import Decimal
 
 
-def fibonacci(n):
+def fibonacci(n, cpp=True, recursion=False):
     """Calculates the fibonacci number of order n.
 
     Parameters
@@ -17,8 +17,34 @@ def fibonacci(n):
         The n'th fibonacci number.
     """
 
-    # Call the executable
-    result = run([f'{Path(__file__).parent.resolve() / "fibonacci"}', str(n)], stdout=PIPE, encoding='utf-8')
+    if cpp:
+        # Call the executable
+        result = run([f'{Path(__file__).parent.resolve() / "fibonacci"}', str(n), str(recursion)], stdout=PIPE, encoding='utf-8')
 
-    # I have to go via float to read scientific notation to int
-    return int(Decimal(result.stdout.strip()))
+        # I have to go via float to read scientific notation to int
+        return int(Decimal(result.stdout.strip()))
+    else:
+        return fibonacci_py(n, recursion)
+
+def fibonacci_py(n, recursion):
+    if recursion:
+        return fibonacci_py_recursion(n)
+    else:
+        return fibonacci_py_iteration(n)
+
+def fibonacci_py_recursion(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci_py_recursion(n-1)+fibonacci_py_recursion(n-2)
+
+def fibonacci_py_iteration(n):
+    n1, n2 = 1, 0
+    for i in range(n):
+        if i < 2:
+            n0 = i+1
+        else:
+            n0 = n1+n2
+            n2 = n1
+            n1 = n0
+    return n0
